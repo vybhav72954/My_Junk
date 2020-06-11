322660828572195e9c565486427cef8e9364004e
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-06-11 17:19:55 +0000

    Update Image.py

--- Projects/Image.py
+++ Projects/Image.py
@@ -1,18 +1,32 @@
-#!/usr/bin/env python3
-import csv
+import tensorflow as tf
+import os
+import numpy as np
 
-def read_employees(filename):
-   csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
-   employee_file = csv.DictReader(open(filename), dialect = 'empDialect')
-   employee_list = []
-   for data in employee_file:
-     employee_list.append(data)
-   return employee_list
+from matplotlib import pyplot as plt
+%matplotlib inline
 
-def process_data(employee_list):
-   department_list = []
-   for employee_data in employee_list:
-     department_list.append(employee_data['Department'])
-   department_data = {}
-   for department_name in set(department_list):
+if not os.path.isdir('models'):
+    os.mkdir('models')
+    
+print('TensorFlow version:', tf.__version__)
+print('Is using GPU?', tf.test.is_gpu_available())
 
+def get_three_classes(x, y):
+    indices_0, _ = np.where(y == 0.)
+    indices_1, _ = np.where(y == 1.)
+    indices_2, _ = np.where(y == 2.)
+
+    indices = np.concatenate([indices_0, indices_1, indices_2], axis=0)
+    
+    x = x[indices]
+    y = y[indices]
+    
+    count = x.shape[0]
+    indices = np.random.choice(range(count), count, replace=False)
+    
+    x = x[indices]
+    y = y[indices]
+    
+    y = tf.keras.utils.to_categorical(y)
+    
+    return x, y
