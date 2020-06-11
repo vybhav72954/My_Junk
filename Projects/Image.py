205c691beeb21a21439155ec8a20b44b111f5059
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-06-11 18:29:23 +0000

    Update Image.py

--- Projects/Image.py
+++ Projects/Image.py
@@ -59,3 +59,37 @@ def show_random_examples(x, y, p):
     
 show_random_examples(x_train, y_train, y_train)
 show_random_examples(x_test, y_test, y_test)
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from tensorflow.keras.layers import Dropout, Flatten, Input, Dense
+
+
+
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from tensorflow.keras.layers import Dropout, Flatten, Input, Dense
+
+def create_model():
+    def add_conv_block(model, num_filters):
+        model.add(Conv2D(num_filters, 3, activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(Conv2D(num_filters, 3, activation='relu'))
+        model.add(MaxPooling2D(pool_size=2))
+        model.add(Dropout(0.5))
+        return model
+        
+    model = tf.keras.models.Sequential()
+    model.add(Input(shape=(32, 32, 3)))
+    
+    model = add_conv_block(model, 32)
+    model = add_conv_block(model, 64)
+    model = add_conv_block(model, 128)
+    
+    model.add(Flatten())
+    model.add(Dense(3, activation = 'softmax'))
+    
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adam', metrics=['accuracy']
+    )
+    return model
+model = create_model()
+model.summary()
